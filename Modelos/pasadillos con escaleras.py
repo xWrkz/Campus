e7eb2d3df7d43be0b803b8e2f86be3303ed08ea1
xWrkz <marcogamez768@gmail.com>
2024-11-08 20:26:30 -0500

    Pasadizos V2.0 con puertas y exterior de baño

--- Modelos/pasadillos con escaleras.py
+++ Modelos/pasadillos con escaleras.py
@@ -17,12 +17,16 @@ def init():
 def draw_walls():
     glColor3f(1.0, 1.0, 1.0)  # Color de las paredes
     wall_data = [
-        {'pos': (-5, 2.5, -7.5), 'scale': (79.0, 5.0, 0.4)}, # pared larga
-        {'pos': (0.0, 2.5, 7.5), 'scale': (79.0, 5.0, 0.4)}, # pared larga
+        {'pos': (-9, 2.5, -7.5), 'scale': (72.0, 5.0, 0.4)}, # pared larga parte 1
+        {'pos': (45, 2.5, -7.5), 'scale': (10.0, 5.0, 0.4)}, # pared larga parte 2
+        {'pos': (0.0, 2.5, 7.5), 'scale': (80.0, 5.0, 0.4)}, # pared larga
         {'pos': (-45, 2.5, 0), 'scale': (10.0, 5.0, 0.4)},   # división
         {'pos': (45, 2.5, 0), 'scale': (10.0, 5.0, 0.4)},    # división
         {'pos': (-50.0, 2.5, 3.8), 'scale': (0.4, 5.0, 7.8)}, # pared ancha
-        {'pos': (50.0, 2.5, 0), 'scale': (0.4, 5.0, 15.4)}   # pared ancha
+        {'pos': (50.0, 2.5, 0), 'scale': (0.4, 5.0, 15.4)},  # pared ancha
+        {'pos': (40.0, 2.5, -10), 'scale': (0.4, 5.0, 5)}, # pared baño 1
+        {'pos': (27.0, 2.5, -10), 'scale': (0.4, 5.0, 5)},   # pared baño 2
+        {'pos': (33.5, 2.5, -12.5), 'scale': (13.5, 5.0, 0.4)}, # pared baño 3
     ]
     for data in wall_data:
         glPushMatrix()
@@ -36,9 +40,9 @@ def draw_floor1(position=(0, 0, 0)):
     glPushMatrix()
     glTranslatef(*position)  # Mover el piso a la posición deseada
     glBegin(GL_QUADS)
-    glVertex3f(-50.1, 0.0, -3.8)
-    glVertex3f(40, 0.0, -3.8)
-    glVertex3f(40, 0.0, 3.8)
+    glVertex3f(-50.1, 0.0, -9)
+    glVertex3f(50, 0.0, -9)
+    glVertex3f(50, 0.0, 3.8)
     glVertex3f(-50.1, 0.0, 3.8)
     glEnd()
     glPopMatrix()
@@ -87,6 +91,57 @@ def draw_stairs(position, steps=5, reverse=False, step_width=5.0, step_depth=2.0
         glutSolidCube(1)
         glPopMatrix()
 
+def draw_puerta(position, color=(0.55, 0.27, 0.07), door_size=(3.0, 4.0, 0.1), rotation=0):
+    glPushMatrix()
+    glTranslatef(*position)
+    glColor3f(*color)
+    glPushMatrix()
+    if rotation != 0:
+        glRotatef(rotation, 0, 1, 0)  # Rotar la puerta si es necesario
+    glScalef(*door_size)
+    glutSolidCube(1)  # Dibuja la puerta
+    glPopMatrix()
+    glPopMatrix()
+
+def draw_puerta_con_cartel(position, door_size=(2.0, 4.0, 0.1), sign_size=(1.0, 0.5, 0.1)):
+    glPushMatrix()
+    glTranslatef(*position)
+    glColor3f(0.55, 0.27, 0.07)  # Color de la puerta (marrón)
+    glPushMatrix()
+    glScalef(*door_size)
+    glutSolidCube(1)  # Dibuja la puerta
+    glPopMatrix()
+
+    # Dibuja el cartel al lado de la puerta
+    glColor3f(1.0, 1.0, 0.0)  # Color del cartel (amarillo)
+    glPushMatrix()
+    glTranslatef(sign_size[0] * 1.5, 1, 0)  # Posicionar el cartel al lado de la puerta
+    glScalef(*sign_size)
+    glutSolidCube(1)  # Dibuja el cartel
+    glPopMatrix()
+    glPopMatrix()
+
+def draw_puertas_con_carteles():
+    # Puertas en la pared larga del frente
+    draw_puerta_con_cartel(position=(27, 2.0, 7.2))  # Puerta 1
+    draw_puerta_con_cartel(position=(12, 2.0, 7.2))  # Puerta 2
+    draw_puerta_con_cartel(position=(-4, 2.0, 7.2))  # Puerta 3
+    draw_puerta_con_cartel(position=(-20, 2.0, 7.2))  # Puerta 4
+    draw_puerta_con_cartel(position=(-38, 2.0, 7.2))  # Puerta 5
+
+    # Puertas en la pared larga de atrás
+    draw_puerta_con_cartel(position=(21, 2.0, -7.2))  # Puerta 6
+    draw_puerta_con_cartel(position=(5, 2.0, -7.2))  # Puerta 7
+    draw_puerta_con_cartel(position=(-13, 2.0, -7.2))  # Puerta 8
+    draw_puerta_con_cartel(position=(-30, 2.0, -7.2))  # Puerta 9
+
+    # Nuevas puertas agregadas
+    # Puerta gris sin cartel
+    draw_puerta(position=(-42, 2.0, -7.2), color=(0.5, 0.5, 0.5))  # Color gris
+    # Puertas marrones sin cartel, rotadas
+    draw_puerta(position=(27.2, 2.0, -9.5), color=(0.55, 0.27, 0.07), rotation=90)  # Puerta rotada
+    draw_puerta(position=(39.8, 2.0, -9.5), color=(0.55, 0.27, 0.07), rotation=90)  # Puerta rotada
+
 def draw_scene():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
@@ -97,6 +152,8 @@ def draw_scene():
     draw_floor1(position=(0, 0, -3.8))
     draw_floor2(position=(0, 0, 3.8))
     draw_walls()
+    draw_puertas_con_carteles()  # Agregar las puertas con carteles
+
     draw_stairs(position=(-47.5, -5.1, 1), steps=10)  # Escaleras a la izquierda
     draw_stairs(position=(47.5, -5.1, 1), steps=10)   # Escaleras a la derecha
     draw_stairs(position=(-42.5, -2.5, 14.5), steps=10, reverse=True)  # Escaleras inversas a la izquierda
